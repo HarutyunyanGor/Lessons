d20166c5649ffa567c4bff49ef82db7ea82bf715
John Doe <harutyunyangor227@gmail.com>
2025-01-02 00:16:01 +0400

    new methods

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,374 +1,183 @@
 package org.example;
 
+import javax.naming.InsufficientResourcesException;
 import java.util.*;
 
 
 public class Main {
-
     public static void main(String[] args) {
-
-
         Scanner sc = new Scanner(System.in);
         Random rand = new Random();
-//        int[] list = new int[10];
-//        for (int i = 0; i < list.length; i++) {
-//            list[i] = rand.nextInt(101);
-//            System.out.print(list[i] + " ");
-//        }
-// a
-//        int max=list[0];
-//        for (int i = 0; i <list.length; i++) {
-//            if(max<list[i])
-//                max = list[i];
-//        }
-//        System.out.println();
-//        System.out.println("max is: "+ max);
-//        double avarege=0;
-//        for (int i = 0; i < list.length ; i++) {
-//            avarege+=list[i]/list.length;
-//        }
-//        System.out.println();
-//        System.out.println("AVAREGE " + avarege);
-//
-//        int[] arr = {72, 89, 3, 5, 7, 8, 3, 21, 64, 2};
-//        for (int i = 0; i < arr.length; i++) {
-//            for (int j = i; j < arr.length; j++) {
-//                if (arr[i] > arr[j]) {
-//                    int tiv = arr[i];
-//                    arr[i] = arr[j];
-//                    arr[j] = tiv;
-//                }
-//            }
-//        }
-//        for(int i:arr)
-//                    System.out.print(i + " ");
-//        int[] arr = {72, 89, 3, 5, 7, 8, 3, 21, 64, 2};
-//        for (int i = 0; i < arr.length; i++) {
-//            for (int j = i; j < arr.length; j++) {
-//                if (arr[i] > arr[j]) {
-//                    int tiv = arr[i];
-//                    arr[i] = arr[j];
-//                    arr[j] = tiv;
-//                }
-//            }
-//        }
-//        for (int i : arr) {
-//            System.out.print(i+" ");
-//        }
-//        int[] arr = {78, 89, 3, 5, 5, 5, 3, 21, 64, 2};
-//        int temp = 0;
-//        for (int i = 0; i < arr.length; i++) {
-//            for (int j = i; j < arr.length; j++) {
-//                if (arr[i] > arr[j]) {
-//                    int tiv = arr[i];
-//                    arr[i] = arr[j];
-//                    arr[j] = tiv;
-//                }
-//            }
-//        }
-//        for (int i:arr) {
-//            System.out.print(i+" ");
-//
-//        }
-//        int max_count = 0;
-//        int temp_count = 0;
-//        for (int i = 0; i < arr.length - 1; i++) {
-//            if (arr[i] == arr[i + 1]) {
-//                temp_count ++;
-//            } else {
-//                if (temp_count > max_count) {
-//                    max_count = temp_count;
-//                    temp_count = 1;
-//                } else {
-//                    temp_count = 1;
-//
-//                }
-//
-//            }
-//        }
-//            System.out.println();
-//        System.out.print(max_count+" ");
-//        int[] arr = {78, 89, 3, 5, 5, 5, 3, 21, 64, 2};
-//        for (int i = 0; i < arr.length; i++) {
-//            for (int j = i; j < arr.length; j++) {
-//                if (arr[i] > arr[j]) {
-//                    int tmp = arr[i];
-//                    arr[i] = arr[j];
-//                    arr[j] = tmp;
-//                }
-//            }
-//        }
-//        int temp=0;
-//        int max=0;
-//        for (int i = 0; i <arr.length-1 ; i++) {
-//            if(arr[i]==arr[i+1]){
-//                temp+=1;
-//            }else {
-//                if(temp>max){
-//                    max=temp;
-//                    temp=1;
-//                }
-//            }
-//        }
-//        System.out.println(max);
-//        int[] arr={2,1,1,3,4,2};
-//        int step=0;
-//        int temp=0;
-//        for (int i = 0; i < arr.length ; i++) {
-//            step=arr[i+step];
-//            temp+=step;
-//            System.out.println(temp);
-//            if(temp==0){
-//            System.out.println("false");
-//            break;}
-//            if (temp> arr.length){
-//                System.out.println("false");
-//                break;}
-//        }
-//        int count_un = args.length;
-//        int count_ch = 0;
-//        String[] arr = {"Baarev"};
-//        for (int i = 0; i < arr.length(); i++) {
-//            if (arr.charAt(i) != arr.charAt(i)) {
-//                count_un--;
-//            } else {
-//                count_ch++;
-//            }
-//            System.out.println(count_un);
-//        }
-//        ArrayList<String> arr = new ArrayList<>();
-//        arr.add("Barev");
-//        String s = "Barev", sub = "Barev";
-//        int count = 0;
-//        int count_0 = 0;
-//        if (s.indexOf(sub) != -1) {
-//            count++;
-//        } else {
-//            count_0++;
-//        }
-//        System.out.println(count_0);
-//        ArrayList<Integer> arr = new ArrayList<>();
-//         arr.add(1);
-//        arr.add(2);
-//        arr.add(3);
-//        arr.add(4);
-//        arr.add(5);
+//        ----------------------------------------------46 shaxmat-----------------------------------
+//        System.out.println("Enter letter");
+//        String letter = sc.next();
 //        System.out.println("Enter number");
-//        int num= sc.nextInt();
-//        ArrayList<Integer> clone = (ArrayList<Integer>) arr.clone();
-//        System.out.print(clone.get(5-num)+" " +clone.get(5-1));
-//            arr.add(j);}1
-
-//        System.out.println(clone);
-//
-//        int[] arr = {5, -1, 36, 72, -1, -1, 13, 4, 7};
-//        for (int i = 0; i < arr.length; i++) {
-//            for (int j = i; j < arr.length; j++) {
-//                if (arr[i] > arr[j]) {
-//                    int temp = arr[i];
-//                    arr[i] = arr[j];
-//                    arr[j] = temp;
-//                }
+//        int number = sc.nextInt();
+//        ArrayList<Character> letters = new ArrayList<>();
+//        letters.add('0');
+//        for (int i = 97; i <104 ; i++) {
+//            letters.add((char)i);}
+//        int num = 0;
+//        for (int i = 0; i < letter.length(); i++) {
+//            num = (letters.indexOf(letter.charAt(i)));
+//        }
+//        if (num % 2 == 0 && number % 2 == 0) {
+//            System.out.println("black");
+//        } else if (number % 2 != 0 && num % 2 == 0) {
+//            System.out.println("white");
+//        } else {
+//            System.out.println("black");
+//        }
+//        -------------------------------------63 mijin tvabanakan---------------------------------------------
+//        ArrayList<Integer> list = new ArrayList<>();
+//        int number=0;
+//        while (true) {
+//            System.out.println("Enter number");
+//            int num = sc.nextInt();
+//            if (num == 0) {
+//                break;
+//            } else {
+//                list.add(num);
+//                number+=num;}}
+//        System.out.println(number/list.size());
+//        ---------------------------------69 gazananoci tomser--------------------------------------
+//        System.out.println("enter 0 for end");
+//        double sum = 0;
+//        while (true) {
+//            System.out.println("Enter age");
+//            int age = sc.nextInt();
+//            if (age == 0) {
+//                break;}
+//            if (age == 1 && age == 2) {
+//                    sum += 0;}
+//            if (age >= 3 && age <= 12) {
+//                sum += 14;
+//            }
+//            if (age >= 13 && age <= 65) {
+//                sum += 23;
+//            }
+//            if (age > 65) {
+//                sum += 18;
 //            }
 //        }
-//        for (int i : arr) {
+//        System.out.println(sum+"$");
+//        ------------------------------72 fizz buzz------------------------------------------
+//        for (int i = 1; i < 101; i++) {
 //            System.out.println(i);
-//
+//            if (i % 3 == 0 && i % 5 == 0) {
+//                System.out.println("fizz-buzz");}
+//            else if (i % 3 == 0) {
+//                System.out.println("fizz");}
+//               else  if (i % 5 == 0) {
+//                    System.out.println("buzz");}
+//                    }
+//        ---------------------------------75 palindrom------------------------
+//        System.out.println("enter text");
+//        String str = sc.next();
+//        boolean palindrome = false;
+//        for (int i = 0; i < str.length(); i++) {
+//            for (int j = str.length() - 1; j > -1; j--) {
+//                if (str.charAt(j) == str.charAt(i)) {
+//                    palindrome = (true);
+//                } else palindrome = (false);
+//            }
 //        }
-//        int max = 0;
-//        for (int i = 0; i < arr.length - 1; i++) {
-//            for (int j = i; j < arr.length; j++) {
-//                if (arr[i] == -1 || arr[j] == -1)
-//                    continue;
-//                    if (arr[i] > arr[j]) {
-//                        int temp = arr[i];
-//                        arr[i] = arr[j];
-//                        arr[j] = temp;
+//        if (palindrome = true) {
+//            System.out.println("its palindrom");
+//        } else {
+//            System.out.println("not palindrom");
+//        }
+//        ----------------------------nuyn 75 rekursiyayov---------------------------------------
+//        System.out.println("Enter text");
+//        String s = sc.next();
+//        System.out.println(name(s)?"palindrom e":"palindrom che");
+//    }
+//    private static boolean name(String s) {
+//        if (s.length() == 1 || s.length() == 0)
+//            return true;
+//        if (s.charAt(0) == s.charAt(s.length()-1))
+//        return name(s.substring(+1, s.length() - 1));
+//        return false;
+//        ------------------------------82 -----------------------------
+//        System.out.println("Enter number");
+//        int num = sc.nextInt();
+//        ArrayList<Integer> list = new ArrayList<>();
+//        while (true) {
+//            if (num == 0) {
+//                break;
+//            } else {
+//                list.add(num / 2);
+//                num=num/2;
+//            }
+//        }
+//        ArrayList<Integer>list2=new ArrayList<>();
+//        for (int i = 0; i <list.size() ; i++) {
+//            list2.add(list.get(i)%2);
+//        }
+//        System.out.println(list2);
+//        ----------------------------------138 mi qich xaxud-----------------------------------------
+//        HashMap<Integer, String> map = new HashMap<>();
+//        map.put(1, ".,?!:");
+//        map.put(2, "ABC");
+//        map.put(3, "DEF");
+//        map.put(4, "GHI");
+//        map.put(5, "JKL");
+//        map.put(6, "MNO");
+//        map.put(7, "PQRS");
+//        map.put(8, "TUV");
+//        map.put(9, "WXYZ");
+//        map.put(0, " ");
+//        System.out.print("Enter name: ");
+//        String name = sc.nextLine();
+//        for (int i = 0; i < name.length(); i++) {
+//            for (int key : map.keySet()) {
+//                String letter = String.valueOf(Character.toUpperCase(name.charAt(i)));
+//                if (map.get(key).contains(letter)) {
+//                    for (int j = 0; j <i+1; j++) {
+//                        System.out.print(key);
+//
 //                    }
 //                }
 //            }
-//            for (int i : arr)
-//                System.out.print(i+" ");
-//        int value=10, value2=12, value3=34, num1,num2,smallest;
-//num1 = (value < value2)?value:value2;
-//num2= (value2 < value3)?value2:value3;
-//smallest=(num1<num2)?num1:num2;
-//        System.out.println(smallest);\
-//        String text = "BarevB";
-//        ArrayList<Character> myArr = new ArrayList<>();
-//        for (int i = 0; i < text.length(); i++) {
-//            if (myArr.contains(text.charAt(i))) {
-//                continue;
-//            } else myArr.add(text.charAt(i));
 //        }
-//        System.out.println(myArr);
-//        System.out.println(myArr.size());
-//        String text = "Hello";
-//        ArrayList<Character> myArr = new ArrayList<>();
+//        -----------------------------------145 Erudit-----------------------------------------
+//        HashMap<Integer, String> map = new HashMap<>();
+//        map.put(1, "AEILNORSTU");
+//        map.put(2, "DG");
+//        map.put(3, "BCMP");
+//        map.put(4, "FHVWY");
+//        map.put(5, "K");
+//        map.put(8, "JX");
+//        map.put(10, "QZ");
+//        System.out.println("Enter text");
+//        String text = sc.next();
+//        int num=0;
 //        for (int i = 0; i < text.length(); i++) {
-//            if (myArr.contains(text.charAt(i))) {
-//                continue;
-//            } else myArr.add(text.charAt(i));
-//        }
-//        System.out.println(myArr);
-//        System.out.println(myArr.size());
-//        ArrayList<Integer> num=new ArrayList<>();
-//        num.add(1);
-//        num.add(2);
-//        num.add(3);
-//        num.add(4);
-//        num.add(5);
-//        int step= 3;
-//        for (int i= 0; i <step; i++) {
-//           int x= num.removeLast();
-//            num.addFirst(x);
+//            for (int key : map.keySet()) {
+//                String letter = String.valueOf(Character.toUpperCase(text.charAt(i)));
+//                if (map.get(key).contains(letter)) {
+//                    for (int j = 0; j < 1; j++) {
+//                        num+=key;
+//                    }
+//                }
+//
+//            }
 //        }
 //        System.out.println(num);
-//        System.out.println("Enter Number");
-//        int number = sc.nextInt();
-//        ArrayList<Integer>numberArray=new ArrayList<>();
-//        int sum=0;
-//        for (int i = 1; i <number ; i++) {
-//            if(number%i==0){
-//                sum+=i;}}
-//            if (sum ==number){
-//                System.out.println("idealakan");}
-//        else System.out.println("sovorakan");
-//        System.out.println("Enter Number");
-//        int number = sc.nextInt();
-//        ArrayList<Integer> numberArray = new ArrayList<>();
-//        int sum = 0;
-//        for (int i = 1; i < number; i++) {
-//            if (number % i == 0)
-//            {
-//                sum += i;
-//            }
-//        }
-//        if (sum == number) {
-//            System.out.println("idealakan");
-//        } else System.out.println("Sovorakan");
-//        int[]arr={2,54,6,47,2,33,4};
-//        for (int i = 0; i <arr.length ; i++) {
-//            for (int j = i; j <arr.length; j++) {
-//                if (arr[i] > arr[j]){
-//                    int temp = arr[i];
-//                    arr[i]=arr[j];
-//                    arr[j]=temp;
-//                }
-//            }
-//        }
-//        for (int i:arr) {
-//            System.out.print(i+" ");
+//        --------------------------------173 summa----------------------------------------
+//        System.out.println(rekurs(0));
+//    }
+//    static int rekurs(int sum) {
+//        Scanner sc = new Scanner(System.in);
 //        System.out.println("Enter number");
-//        int number = sc.nextInt();
-//        int sum=0;
-//        ArrayList<Integer>numberarray=new ArrayList<>();
-//        for (int i = 1; i <number ; i++) {
-//            if (number % i == 0){
-//                sum+=i;
-//            }
-//        }
-//        if (sum==number) {
-//            System.out.println("Idealakan");
-//        }else System.out.println("Sovorakan");
-//        }
-//        System.out.println("Enter number");
-//        int num = sc.nextInt();
-//        for (int i = 0; i < num; i++) {
-//            for (int j = 0; j < num * 2 - 1; j++) {
-//                if (j < (num * 2 - 1) / 2 - i || j > (num * 2 - 1) / 2 + i) {
-//                    System.out.print(" ");
-//                } else {
-//                    System.out.print("#");
-//
-//                }
-//            }
-//            System.out.println();
-//        }
-
-
-//            int[] arr = {32,12,412,12,33,2,5,712,};
-//            {
-//                for (int i = 0; i < arr.length; i++) {
-//                    for (int j = i; j < arr.length; j++) {
-//                        if (arr[i] > arr[j]) {
-//                            int num = arr[i];
-//                            arr[i] = arr[j];
-//                            arr[j] = num;
-//                        }
-//                    }
-//                }
-//                for (int i : arr) {
-//                    System.out.print(i + " ");
-//
-//                }
-//            }
-//        int user = 0;
-//        int pc = 0;
-//        System.out.println("Enter number");
-//        int usernum = sc.nextInt();
-//        int pcnum = rand.nextInt(1, 5);
-//        if (usernum == pcnum) {
-//            user += 1;
-//            System.out.println("Win user");
+//        int temp = sc.nextInt();
+//        if (temp != 0) {
+//            return rekurs(sum += temp );
 //        } else {
-//            pc += 1;
-//            System.out.println("Win pc number is "+pcnum);
+//            return sum;
 //        }
-//        HashMap<String,Integer> map = new HashMap<>();
-//        map.put ("A",1);
-//        map.put ("B",2);
-//        map.put ("C",2);.,?!:
-//        HashMap<String,ArrayList> map2 = new HashMap<>();
-//        for (int i = 0; i < map.size() ; i++) {
-//            if (map.get(i).intValue()==map.get(i+1).intValue()){
-//                System.out.println(map);
-//            }
-// //////////////////////////////////////////////////////////////////////////////
-//        HashMap<Integer,String> phone = new HashMap();
-//        phone.put( 1,".,?!:");
-//        phone.put( 2,"ABC");
-//        phone.put( 3,"DEF");
-//        phone.put( 4,"GHI");
-//        phone.put( 5,"JKL");
-//        phone.put( 6,"MNO");
-//        phone.put( 7,"PQRS");
-//        phone.put( 8,"TUV");
-//        phone.put( 9,"WXYZ");
-//        phone.put(0, " ");
-//        "HELLO"
-//        System.out.print("Enter text: ");
-//        int num = sc.nextInt();
-//        for(int i = 0; i < phone.size() ; i ++){
-//            if (phone.containsKey(num))
-//            System.out.print(phone.keySet());
-//        }
-//        ----------------------XNDIR 138-----------------------
-        HashMap<Integer, String> map = new HashMap<>();
-        map.put(1, ".,?!:");
-        map.put(2, "ABC");
-        map.put(3, "DEF");
-        map.put(4, "GHI");
-        map.put(5, "JKL");
-        map.put(6, "MNO");
-        map.put(7, "PQRS");
-        map.put(8, "TUV");
-        map.put(9, "WXYZ");
-        map.put(0, " ");
-        System.out.print("Enter name: ");
-        String name = sc.nextLine();
-        int sum = 0;
-        int count = 0;
-        for (int i = 0; i < name.length(); i++) {
-            for (int key : map.keySet()) {
-                String letter = String.valueOf(Character.toUpperCase(name.charAt(i)));
-                if (map.get(key).contains(letter)) {
-                    for (int j = 0; j < 2; j++) {
-
-                        System.out.print(key);
-                    }
-                }
-            }
-        }
+//    }
+//}
+//---------------------------------------------------------------------------------------------
     }
 }
